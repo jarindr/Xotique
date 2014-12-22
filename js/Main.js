882b785cc15a21ee894847ce841ac1df0437edbe
jarindr <jarindr23@gmail.com>
2014-12-23 03:15:09 +0700

    main,about final

--- js/Main.js
+++ js/Main.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
     var check = true;
     $(".nav-menu").click(function () {
         if (check) {
-            $(".hidden-nav").animate({right: "0", opacity: 1, top: "80"}, 0, function () {
+            $(".hidden-nav").animate({right: "0", opacity: 1, top: "100"}, 0, function () {
                 check = false;
             });
         }else{
-            $(".hidden-nav").animate({right: "-200", opacity: 0, top: "80"}, 0, function () {
+            $(".hidden-nav").animate({right: "-200", opacity: 0, top: "100"}, 0, function () {
                 check = true;
             });
         }
